14894c0a247013f9233c257e3221d3c5c1779e19
3λΞĐ <git61@3yed.eu.org>
2024-09-27 06:06:28 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1131,7 +1131,7 @@ function generateRemark(index, port, address, cleanIPs, protocol, configType) {
         ? addressType = 'Clean IP'
         : addressType = isDomain(address) ? 'Domain': isIPv4(address) ? 'ipv4' : isIPv6(address) ? 'ipv6' : '';
 
-    return `${index} - ${protocol}${type} - ${addressType} : ${port} |♨️`;
+    return `${index} - ${protocol}${type} - ${addressType} : ${port} |⚕️`;
 }
 
 function isDomain(address) {
